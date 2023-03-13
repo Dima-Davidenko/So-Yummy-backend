4ee3899bd79ceb796359608faf965ec9347ba73f
Dmytro Davidenko <dima080885@gmail.com>
2023-03-13 13:06:10 +0200

    bugfix getAll - sort algorithm

--- controllers/recipes/getAll.js
+++ controllers/recipes/getAll.js
@@ -2,17 +2,18 @@ const { listRecipeResponse } = require('../../helpers');
 const { Recipe } = require('../../models/recipe');
 
 const getAll = async (req, res) => {
-  const { page = 1, limit = 300, popular = false } = req.query;
+  const { page = 1, limit = 300, sort = false } = req.query;
   const options = {
     skip: (page - 1) * limit,
     limit,
   };
   const sortOpts = {};
-  if (popular) {
+  if (sort === 'popular') {
     sortOpts.popularity = 1;
   } else {
     sortOpts.title = 1;
   }
+  console.log(sortOpts);
   const result = await Recipe.find(null, '-createdAt -updatedAt', options).sort(sortOpts);
   const recipes = listRecipeResponse(result, req.user._id);
 
