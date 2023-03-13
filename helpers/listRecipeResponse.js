ace950ffc29942beabfc2b2633d3664744cf3aa0
Dmytro Davidenko <dima080885@gmail.com>
2023-03-13 13:26:25 +0200

    add recipe's description

--- helpers/listRecipeResponse.js
+++ helpers/listRecipeResponse.js
@@ -1,12 +1,13 @@
 const listRecipeResponse = (response, userId) => {
   const recipes = response.map(recipe => {
-    const { _id, title, category, thumb, time, popularity, likes, favorites } = recipe;
+    const { _id, title, category, thumb, time, popularity, likes, favorites, description } = recipe;
     const like = likes.includes(userId);
     const favorite = favorites.includes(userId);
     return {
       _id,
       title,
       category,
+      description,
       thumb,
       time,
       popularity,
